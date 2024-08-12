539f3ea60171999ed90a381a7502fd6672250aaa
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-12 18:51:59 +0530

    Added Login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,46 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { GithubIcon } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import googleIcon from '../../../public/google.svg';
+
 export default function Login() {
   return (
-    <div>
-      <h1>Hello from Index page</h1>
+    <div className="mt-24 rounded-md bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+      <form action="">
+        <h1 className="text-3xl font-semibold text-white">Login</h1>
+        <div className="space-y-4 mt-5">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+          />
+          <Button
+            type="submit"
+            variant="destructive"
+            className="w-full bg-[#e50914]">
+            Sign Up
+          </Button>
+        </div>
+      </form>
+
+      <div className="text-gray-500 text-sm mt-2 text-center">
+        Don't have an account?{' '}
+        <Link className="text-white hover:underline" href="/sign-up">
+          Sign up
+        </Link>
+      </div>
+
+      <div className="flex w-full justify-center items-center gap-x-3 mt-6">
+        <Button variant="outline" size="icon">
+          <GithubIcon className="w-6 h-6" />
+        </Button>
+        <Button variant="outline" size="icon">
+          <Image src={googleIcon} alt="Google Icon" className="w-8 h-8" />
+        </Button>
+      </div>
     </div>
   );
 }
