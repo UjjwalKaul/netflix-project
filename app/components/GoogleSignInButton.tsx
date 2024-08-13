2ba27d14118b462a841a007e25e82a49f2e51538
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-13 16:31:13 +0530

    Added google sign in

--- app/components/GoogleSignInButton.tsx
+++ app/components/GoogleSignInButton.tsx
@@ -1,10 +1,16 @@
 'use client';
 import googleIcon from '../../public/google.svg';
 import { Button } from '@/components/ui/button';
+import { signIn } from 'next-auth/react';
 import Image from 'next/image';
 const GoogleSignInButton = () => {
   return (
-    <Button variant="outline" size="icon">
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => {
+        signIn('google');
+      }}>
       <Image priority src={googleIcon} alt="Google Icon" className="w-8 h-8" />
     </Button>
   );
