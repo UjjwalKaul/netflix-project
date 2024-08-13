9202d3e7eab487ed2f271f317e4a881119f91de6
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-13 16:41:18 +0530

    Email auth added

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -15,7 +15,7 @@ export default async function Signup() {
   }
   return (
     <div className="mt-24 rounded-md bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
-      <form action="">
+      <form method="post" action="/api/auth/signin">
         <h1 className="text-3xl font-semibold text-white">Signup</h1>
         <div className="space-y-4 mt-5">
           <Input
