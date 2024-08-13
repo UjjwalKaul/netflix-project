3ef677eee7d95dfbd2716b7b9a5639bd8b6f2a9c
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-13 17:25:55 +0530

    MovieVideo comp

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import prisma from '../utils/db';
 
 async function getData() {
@@ -17,7 +18,27 @@ async function getData() {
 }
 const MovieVideo = async () => {
   const data = await getData();
-  return <div>MovieVideo</div>;
+  return (
+    <div className="h-[55vh] lg:h-[60vh] w-full flex justify-start items-center">
+      <video
+        poster={data?.imageString}
+        autoPlay
+        muted
+        loop
+        src={data?.videoSource}
+        className="w-full absolute top-0 left-0 h-[60vh] object-cover -z-10 brightness-[60%]"></video>
+      <div className="absolute w-[90%] lg:w-[40%] mx-auto">
+        <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold">
+          {data?.title}
+        </h1>
+        <p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
+        <div className="flex gap-x-3 mt-4">
+          <Button>See more</Button>
+          <Button>Learn More</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieVideo;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,9 +2,10 @@ import MovieVideo from '../components/MovieVideo';
 
 const HomePage = () => {
   return (
-    <>
+    <div className="p-5 lg:p-0">
       <MovieVideo />
-    </>
+      <h1 className="text-3xl font-bold">Recently Added</h1>
+    </div>
   );
 };
 
