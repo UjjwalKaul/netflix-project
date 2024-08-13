3cbb473cb4c92d04167450348ea94d10972b3cde
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-13 20:40:33 +0530

    fixed login error

--- app/components/GithubSignInButton.tsx
+++ app/components/GithubSignInButton.tsx
@@ -2,12 +2,14 @@
 import { Button } from '@/components/ui/button';
 import { GithubIcon } from 'lucide-react';
 import { signIn } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
 const GithubSignInButton = () => {
   return (
     <Button
       onClick={() => {
         signIn('github');
+        redirect('/home');
       }}
       variant="outline"
       size="icon">
--- app/components/GoogleSignInButton.tsx
+++ app/components/GoogleSignInButton.tsx
@@ -3,6 +3,7 @@ import googleIcon from '../../public/google.svg';
 import { Button } from '@/components/ui/button';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import { redirect } from 'next/navigation';
 const GoogleSignInButton = () => {
   return (
     <Button
@@ -10,6 +11,7 @@ const GoogleSignInButton = () => {
       size="icon"
       onClick={() => {
         signIn('google');
+        redirect('/home');
       }}>
       <Image priority src={googleIcon} alt="Google Icon" className="w-8 h-8" />
     </Button>
