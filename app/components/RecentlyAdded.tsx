4b63a85e08cb244fd6fad0741a87ce1d48df0a76
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-15 14:56:43 +0530

    Added recent added movies

--- app/components/RecentlyAdded.tsx
+++ app/components/RecentlyAdded.tsx
@@ -1,30 +1,39 @@
+import Image from 'next/image';
 import prisma from '../utils/db';
-
 async function getData() {
   const data = await prisma.movie.findMany({
     select: {
       id: true,
       overview: true,
-      title: true,
+      WatchLists: true,
       imageString: true,
-      youtubeString: true,
-      age: true,
-      release: true,
-      duration: true,
+      videoSource: true,
     },
     orderBy: {
       createdAt: 'desc',
     },
     take: 4,
   });
-
   return data;
 }
+
 const RecentlyAdded = async () => {
   const data = await getData();
   return (
-    <div>
-      <p>Hello from Component</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
+      {data.map((movie) => {
+        return (
+          <div key={movie.id} className="relative h-48">
+            <Image
+              width={500}
+              height={600}
+              src={movie.imageString}
+              alt="Thumbnail"
+              className="rounded-sm absolute w-full h-full"
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
