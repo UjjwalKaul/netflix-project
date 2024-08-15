33c6b64bbbbcdbf6f8b4654448be5057f4db6837
Ujjawal Kaul <ujjwal171@outlook.com>
2024-08-15 18:37:30 +0530

    Style play button

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -19,11 +19,17 @@ const MovieCard = ({
   watchList,
   watchListId,
   youtubeUrl,
+  year,
+  age,
+  time,
 }: MovieCardProps) => {
   return (
     <>
       <button className="-mt-14">
-        <Play stroke="#e2e8f0" className="h-20 w-20 mt-16 text-sl" />
+        <Play
+          fill="#09090b"
+          className="h-11 w-11 mt-10 text-zinc-950 bg-slate-300 rounded-full p-2"
+        />
       </button>
       <div className="right-5 top-5 absolute z-10">
         {watchList ? (
@@ -43,10 +49,15 @@ const MovieCard = ({
       <div className="p-5 absolute bottom-0 left-0">
         <h1 className="font-bold text-lg line-clamp-1">{title}</h1>
         <div className="flex gap-x-2 items-center">
-          <p>2023</p>
-          <p>12+</p>
-          <p>1.50h</p>
+          <p className="font-normal text-sm">{year}</p>
+          <p className="font-normal border py-0.5 px-1 border-gray-200 rounded-sm text-sm">
+            {age}+
+          </p>
+          <p className="font-normal text-sm">{time}h</p>
         </div>
+        <p className="line-clamp-1 text-sm text-gray-200 font-light">
+          {overview}
+        </p>
       </div>
     </>
   );
